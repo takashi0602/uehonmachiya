15f3bd9ac7cc711f08bc9ae7f20b93911a6f5175
unknown <B7121@oic.osaka.jp>
2018-10-26 17:42:46 +0900

    入庫先

--- resources/views/supplier/ordering/index.blade.php
+++ resources/views/supplier/ordering/index.blade.php
@@ -1,1 +1,42 @@
 <h1>発注一覧</h1>
+
+<table>
+    <tr><td>日付</td><td>発注コード</td><td>発注先</td></tr>
+
+
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td><a href="">詳細</a> </td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td><a href="">詳細</a> </td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td><a href="">詳細</a> </td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td><a href="">詳細</a> </td>
+    </tr>
+
+
+</table>
+
+<tr>
+
+    <a href="">出庫済み一覧</a>
+
+</tr>
