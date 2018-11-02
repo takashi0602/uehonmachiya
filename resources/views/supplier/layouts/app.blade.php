fb2e0125ef1fdce3e946c0f2440738b758a4e0a5
unknown <B7121@oic.osaka.jp>
2018-11-02 16:51:22 +0900

    ヘッダー

--- resources/views/supplier/layouts/app.blade.php
+++ resources/views/supplier/layouts/app.blade.php
@@ -1,1 +1,12 @@
-<h1>ヘッダー</h1>
+
+
+<heaader>
+    <nav>
+        <h1>入庫先名</h1>
+        <ul>
+            <li><a href="{{ url("/supplier/ordering"}}">発注一覧</a></li>
+            <li><a href="{{ url("/supplier/shipped") }}">出庫済み一覧</a></li>
+        </ul>
+    </nav>
+</heaader>
+
