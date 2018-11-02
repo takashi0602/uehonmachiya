29ab72fc788e4469c705e191e15e1a9d662e414d
TO THANH DUY <B7231@oic.osaka.jp>
2018-11-02 17:23:57 +0900

    11/2修正

--- resources/views/supplier/layouts/app.blade.php
+++ resources/views/supplier/layouts/app.blade.php
@@ -1,18 +1,18 @@
 <h1>ヘッダー</h1>
 <form action="">
-<tr>
-    <th><a href="">商品</a></th>
-    <th><a href="">会員</a></th>
-    <th><a href="">入庫先</a></th>
-    <th><a href="">在庫</a></th>
-    <th><a href="">注文</a></th>
-    <th><a href="">発注</a></th>
-    <th><a href="">入庫</a></th>
-    <th><a href="">出庫</a></th>
-    <th><a href="">売上</a></th>
-    <th><a href="">ランキング</a></th>
-    <th><a href="">お問い合わせ</a></th>
+    <tr>
+        <th><a href="">商品</a></th>
+        <th><a href="">会員</a></th>
+        <th><a href="">入庫先</a></th>
+        <th><a href="">在庫</a></th>
+        <th><a href="">注文</a></th>
+        <th><a href="">発注</a></th>
+        <th><a href="">入庫</a></th>
+        <th><a href="">出庫</a></th>
+        <th><a href="">売上</a></th>
+        <th><a href="">ランキング</a></th>
+        <th><a href="">お問い合わせ</a></th>
 
 
-</tr>
+    </tr>
 </form>
