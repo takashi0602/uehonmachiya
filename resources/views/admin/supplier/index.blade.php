2e017740f99ca43363eb7e039183858ef9ecae41
unknown <B7176@oic.osaka.jp>
2018-11-02 15:50:37 +0900

    2018/11/02作業

--- resources/views/admin/supplier/index.blade.php
+++ resources/views/admin/supplier/index.blade.php
@@ -1,1 +1,34 @@
 <h1>入庫先一覧</h1>
+<div><a href="http://127.0.0.1:8000/admin/supplier/add">入庫先の追加</a></div>
+<div>▼詳細検索</div>
+
+<table border="2" rules="all" >
+    <tr>
+        <th>No.(入庫番号)</th>
+        <th>入庫先名</th>
+        <th>入庫先郵便番号</th>
+        <th>入庫先住所</th>
+        <th>入庫先電話番号(TEL)</th>
+        <th>入庫先電話番号(FAX)</th>
+        <th>メールアドレス</th>
+        <th>代表者名</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>OIC問屋</td>
+        <td>1001000</td>
+        <td>大阪市天王寺区上本町</td>
+        <td>090-1234-5678</td>
+        <td>012-441-222</td>
+        <td>oic@gmail.com</td>
+        <td>大阪太郎</td>
+    </tr>
+
+</table>
+
+<ul>
+    <li><a href="">1</a></li>
+    <li><a href="">2</a></li>
+    <li><a href="">X(最後のページ)</a></li>
+
+</ul>
