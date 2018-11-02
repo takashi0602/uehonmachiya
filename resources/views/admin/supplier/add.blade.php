64bfa13ddde44f7b3dd3468c95a7f2206a68d98d
TO THANH DUY <B7231@oic.osaka.jp>
2018-11-02 16:48:51 +0900

    uehonmachi11-02

--- resources/views/admin/supplier/add.blade.php
+++ resources/views/admin/supplier/add.blade.php
@@ -1,1 +1,13 @@
 <h1>入庫先追加</h1>
+
+
+<div>入庫先名</div>
+<div>郵便番号</div>
+<div>入庫先住所</div>
+<div>入庫先TEL</div>
+<div>入庫先FAX</div>
+<div>メールアドレス</div>
+<div>代表者</div>
+<div>パスワード</div>
+
+<input type="button" value="追加">
