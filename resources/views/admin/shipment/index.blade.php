57f3141c37e2b0588a7664ff493cd880369f569c
unknown <B7176@oic.osaka.jp>
2018-11-06 15:53:19 +0900

    11/06作業修正

--- resources/views/admin/shipment/index.blade.php
+++ resources/views/admin/shipment/index.blade.php
@@ -1,1 +1,14 @@
-<h1>出庫一覧</h1>
+@extends('admin.layouts.app')
+            @section('content')
+                <h1>出庫一覧</h1>
+                <div>▼詳細検索</div>
+
+                <table border="2" rules="all" >
+                    <tr>
+                        <th>No</th>
+                        <th>出庫日</th>
+                        <th>会員名</th>
+                        <th> </th>
+                    </tr>
+
+    @endsection
