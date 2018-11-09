f9c956a8b0f33dd839236d978258752f32c24da1
TO THANH DUY <B7231@oic.osaka.jp>
2018-11-09 14:53:08 +0900

    14:52 11/09

--- resources/views/admin/sales.blade.php
+++ resources/views/admin/sales.blade.php
@@ -1,17 +1,31 @@
 @extends('admin.layouts.app')
 @section('content')
 <h1>売上一覧</h1>
-<th>
-    <tr>NO.1(会員番号)</tr>
-    <tr>日付</tr>
-    <tr>商品名</tr>
-    <tr>個数</tr>
-    <tr>合計金額</tr>
-    <tr>利益</tr>
-    <tr>購入者名</tr>
-</th>
-<br>
-<input type="button"value="1">
-<input type="button"value="2">
-<input type="button"value=".....X(最後のページ)">
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">NO(会員番号)</th>
+        <th scope="col">日付</th>
+        <th scope="col">商品名</th>
+        <th scope="col">個数</th>
+        <th scope="col">合計</th>
+        <th scope="col">金額</th>
+        <th scope="col">利益</th>
+        <th scope="col">購入者名</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <th scope="row">1</th>
+        <td>。。。</td>
+        <td>。。。</td>
+        <td>。。。</td>
+        <td>。。。</td>
+        <td>。。。</td>
+        <td>。。。</td>
+        <td>。。。</td>
+    </tr>
+
+    </tbody>
+</table>
     @endsection
