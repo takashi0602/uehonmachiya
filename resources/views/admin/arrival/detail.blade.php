65d8223a9aa3e224fae188d538c7256030c716e7
unknown <B7176@oic.osaka.jp>
2018-11-09 14:53:46 +0900

    11/9作業

--- resources/views/admin/arrival/detail.blade.php
+++ resources/views/admin/arrival/detail.blade.php
@@ -1,14 +1,57 @@
 @extends('admin.layouts.app')
 @section('content')
 <h1>入庫詳細</h1>
-<div>入庫番号</div>
-<div>発注番号</div>
-<div>入庫日</div>
-<div>入庫先</div>
-<div>発注者</div>
-<div>商品番号</div>
-<div>商品名</div>
-<div>個数</div>
-<div>合計金額</div>
-<input type="button" value="戻る">
+
+
+
+<table class="table">
+
+    <tr>
+        <th scope="col">入庫番号</th>
+        <td scope="col"></td>
+        <td></td>
+
+    </tr>
+
+    <tbody>
+    <tr>
+        <th scope="row">発注番号</th>
+        <td></td>
+        <td></td>
+
+    </tr>
+    <tr>
+        <th scope="row">入庫日</th>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row">入庫先</th>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row">発注者</th>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row">商品番号</th>
+        <th scope="row">商品名</th>
+        <th scope="row">個数</th>
+
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>C++プログラミング</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <th scope="row">商品番号</th>
+        <td>1,000</td>
+        <td> </td>
+    </tr>
+    </tbody>
+</table>
+<a href="{{ url('/admin/arrival') }}"><input type="button" value="戻る" ></a>
     @endsection
