fe3295a3c5f2f4353e2d8bf1ee5f433c631d6aac
unknown <B7176@oic.osaka.jp>
2018-11-09 17:38:03 +0900

    11/9最終

--- resources/views/admin/order/detail.blade.php
+++ resources/views/admin/order/detail.blade.php
@@ -2,14 +2,60 @@
 @section('content')
 <h1>注文詳細</h1>
 
-<table>
 
-        <div>会員名</div>
-        <div>郵便番号</div>
-        <div>住所</div>
-        <div>商品番号</div>
-        <div>会計金額</div>
 
+<table class="table">
+        <thead>
+        <tr>
+                <th scope="col">注文番号</th>
+                <td>1</td>
+                <td></td>
+
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+                <th scope="row">注文日</th>
+                <td></td>
+                <td></td>
+
+        </tr>
+        <tr>
+                <th scope="row">会員名</th>
+                <td></td>
+                <td></td>
+
+        </tr>
+        <tr>
+                <th scope="row">郵便番号</th>
+                <td></td>
+                <td></td>
+
+        </tr>
+        <tr>
+                <th scope="row">住所</th>
+                <td></td>
+                <td></td>
+
+        </tr>
+        <tr>
+                <th scope="row">商品番号</th>
+                <th scope="row">商品名</th>
+                <th scope="row">個数</th>
+
+        </tr>
+        <tr>
+                <td>1</td>
+                <td>C++プログラミング</td>
+                <td>1</td>
+        </tr>
+        <tr>
+                <th scope="row">合計金額</th>
+                <td></td>
+                <td></td>
+
+        </tr>
+        </tbody>
 </table>
-<input type="button" value="戻る">
+<th><a href="{{ url('/admin/order') }}"><input type="button" value="戻る"></a></th>
         @endsection
