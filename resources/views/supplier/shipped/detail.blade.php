02461f96c55304416f04bed84b23695cf9814b82
unknown <B7121@oic.osaka.jp>
2018-11-13 16:36:32 +0900

    詳細改訂版

--- resources/views/supplier/shipped/detail.blade.php
+++ resources/views/supplier/shipped/detail.blade.php
@@ -5,55 +5,85 @@
 
 
 
-<table class="table ">
 
-    <tr>
-        <td> 出庫日</td>
-        <td>２０１８年 １２月　３日 </td>
-        <td> </td>
-    </tr>
-    <tr>
-        <td>発注コード</td>
-        <td>１</td>
-        <td></td>
 
-    </tr>
-    <tr>
-        <td>商品コード</td>
-        <td>商品名</td>
-        <td>個数</td>
 
-    </tr>
-    <tr>
-        <td>２０３</td>
-        <td>羅生門</td>
-        <td>１</td>
-    </tr>
-    <tr>
-        <td>発注先名</td>
-        <td>上本町屋</td>
-        <td></td>
+<div class="row">
+    <div class="ml-4 my-5 col-sm-2">出庫日</div>
+    <div class="mr-5 my-5 col-sm-4"> ２０１８年　１２月　３日</div>
 
-    </tr>
-    <tr>
-        <td>発注先住所</td>
-        <td>〒０００-００００</td>
-        <td></td>
 
-    </tr>
-    <tr>
-        <td></td>
-        <td>大阪府・・・</td>
-        <td></td>
+    </div>
+</div>
 
-    </tr>
-    <tr>
-        <td>合計金額</td>
-        <td>１，０００円</td>
-        <td></td>
 
-    </tr>
-</table>
+<div class="row">
+    <div class="ml-4 my-2 col-sm-2">
+       商品コード
+    </div>
+    <div class="my-2 col-sm-2">
+        商品名
+    </div>
+    <div class="my-2 col">
+        個数
+    </div>
+</div>
+</div>
+
+<div class="row">
+    <div class="ml-3 col-sm-2">
+        　　 ２０３
+    </div>
+    <div class="ml-2 col-sm-2">
+        羅生門
+    </div>
+    <div class="mr-5 col-sm-2">
+        　１
+    </div>
+</div>
+
+<div class="row">
+    <div class="ml-4 my-5 col-sm-1">
+        発注先名 　
+    </div>
+    <div class="my-5 col-sm-1">
+        上本町屋
+    </div>
+</div>
+
+<div class="row">
+    <div class="ml-4 my-3 col-sm-1">
+        発注先住所　
+    </div>
+
+    <div class="my-3 col-sm-1">
+       〒０００－００００
+    </div>
+
+</div>
+
+<div class="row">
+    <div class="my-3 col-sm-1">
+
+    </div>
+    <div class="my-3 ml-4 col-sm-1">
+        大阪府・・・
+    </div>
+</div>
+
+<div class="row">
+    <div class="ml-4 my-5 col-sm-1">
+        合計金額　
+    </div>
+    <div class="my-5 col-sm-1">
+        １，０００円
+    </div>
+    <div class="col">
+        　
+    </div>
+</div>
+
+
 <input type="button" value="戻る"style="position: absolute; right: 50px;">
 
 @endsection
