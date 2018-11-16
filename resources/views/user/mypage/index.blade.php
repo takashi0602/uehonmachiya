455420320744cf05b4934aa2e3970dce77aa68f2
unknown <B7121@oic.osaka.jp>
2018-11-16 16:34:01 +0900

    11/16

--- resources/views/user/mypage/index.blade.php
+++ resources/views/user/mypage/index.blade.php
@@ -1,1 +1,15 @@
 <h1>マイページ</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+        </div>
+        <div class="col-m">
+            マイページ
+        </div>
+        <div class="col">
+
+        </div>
+    </div>
+</div>
