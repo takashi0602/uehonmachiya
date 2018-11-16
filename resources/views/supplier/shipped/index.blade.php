965fc8b89363203b6f9f814ca6952ec9dd84dfc6
unknown <B7083@oic.osaka.jp>
2018-11-16 16:09:55 +0900

    更新

--- resources/views/supplier/shipped/index.blade.php
+++ resources/views/supplier/shipped/index.blade.php
@@ -3,6 +3,33 @@
 @section('content')
 <h1>出庫済み一覧</h1>
 
+<!-- Modal -->
+<div class="modal fade bd-example-modal-lg" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">出庫済み詳細</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                出庫日：２０１８年　１２月　３日<br>
+                商品コード：２０３<br>
+                商品名：羅生門　<br>
+                個数：１<br>
+                発注先名：上本町屋<br>
+                発注先住所：〒000-0000 大阪府・・・<br>
+                合計金額：1,000円<br>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
 
 
@@ -21,19 +48,25 @@
         <th scope="row"></th>
         <td></td>
         <td></td>
-        <td><a href="">詳細</a></td>
+        <td><a  class="" data-toggle="modal" data-target="#exampleModalCenter">
+                <ins>詳細</ins>
+            </a></td>
     </tr>
     <tr>
         <th scope="row"></th>
         <td></td>
         <td></td>
-        <td><a href="">詳細</a></td>
+        <td><a  class="" data-toggle="modal" data-target="#exampleModalCenter">
+                <ins>詳細</ins>
+            </a></td>
     </tr>
     <tr>
         <th scope="row"></th>
         <td></td>
         <td></td>
-        <td><a href="">詳細</a></td>
+        <td><a  class="" data-toggle="modal" data-target="#exampleModalCenter">
+                <ins>詳細</ins>
+            </a></td>
     </tr>
     </tbody>
 </table>
