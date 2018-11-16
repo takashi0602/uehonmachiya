456c842305cf7422d796f2fb2e268f1ca94c66f2
unknown <B7083@oic.osaka.jp>
2018-11-16 14:29:22 +0900

    更新

--- resources/views/supplier/ordering/detail.blade.php
+++ resources/views/supplier/ordering/detail.blade.php
@@ -68,7 +68,7 @@
         </div>
     </div>
     <div class="row">
-        <div class="pb-3 col">exit
+        <div class="pb-3 col">
 
             合計金額
         </div>
@@ -76,8 +76,38 @@
             １０００円
         </div>
     </div>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+        発注詳細
+    </button>
 
-
+    <!-- Modal -->
+    <div class="modal fade bd-example-modal-lg" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">発注詳細</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    発注日 １２月１日<br>
+                    発注コード:<br>
+                    商品コード: 203<br>
+                    商品名: 羅生門<br>
+                    個数:　１<br>
+                    発注先名: 上本町屋<br>
+                    発注先住所: 〒０００-００００　大阪府～<br>
+                    合計金額: １０００円<br>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                    <button type="button" class="btn btn-primary">出庫する</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <input type="button" value="戻る">
 
