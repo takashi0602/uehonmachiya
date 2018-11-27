59dfa254abb096930abeb1d27c1f3bfe20e577c3
unknown <B7176@oic.osaka.jp>
2018-11-27 17:37:29 +0900

    在庫一覧エラー

--- app/Http/Controllers/Admin/StockController.php
+++ app/Http/Controllers/Admin/StockController.php
@@ -5,15 +5,27 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Stock;
+use App\Models\Product;
+
 
 
 class StockController extends Controller
 {
-  public function index()
-  {
-    $stocks = Stock::all();
-    return view('admin.stock', [
-      'stocks' => $stocks
-    ]);
-  }
+    public function index()
+    {
+        $products = [];
+        $count = 0;
+        $stocks = Stock::all();
+        dd($stocks);
+        foreach ($stocks as $stock) {
+            $products[] = Product::select('name')->where('id', $stock->product_id)->first();
+        }
+        return view('admin.stock', [
+            'stocks' => $stocks,
+            'products' => $products,
+            'count' => $count
+        ]);
+    }
 }
+
+
