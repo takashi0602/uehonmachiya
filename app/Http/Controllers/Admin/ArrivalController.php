eb7cf85ea9a327cc2c68e13cf7bb246adbfcfddd
unknown <B7176@oic.osaka.jp>
2018-12-11 17:47:16 +0900

    発注・入庫一覧seed追加

--- app/Http/Controllers/Admin/ArrivalController.php
+++ app/Http/Controllers/Admin/ArrivalController.php
@@ -6,21 +6,25 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Arrival;
 use App\Models\Product;
-
+use App\Models\Supplier;
 class ArrivalController extends Controller
 {
   public function index()
   {
-
+      $suppliers = [];
       $products = [];
       $count = 0;
-      $arrivals = Arrival::select("product_id","arrival_id","code")->get();
+      $arrivals = Arrival::select("id","supplier_id","product_id","arrival_id","amount","created_at","status")->get();
       foreach ($arrivals as $arrival) {
-          $products[] = Product::select('id')->where('id', $arrival->product_id)->first();
+          $products[] = Product::select('name')->where('id', $arrival->product_id)->first();
+      }
+      foreach ($arrivals as $arrival) {
+          $suppliers[] = Supplier::select('name')->where('id', $arrival->supplier_id)->first();
       }
     return view('admin.arrival.index', [
       'arrivals' => $arrivals,
         'products' => $products,
+        'suppliers' => $suppliers,
         'count' => $count,
     ]);
   }
--- app/Http/Controllers/Admin/OrderingController.php
+++ app/Http/Controllers/Admin/OrderingController.php
@@ -5,15 +5,29 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Ordering;
+use App\Models\Product;
+use App\Models\Supplier;
 
 class OrderingController extends Controller
 {
   public function index()
   {
-    $ordering = Ordering::select("id","ordering_id","product_id");
-      dd($ordering);
-    return view('admin.order.index', [
-      'ordering' => $ordering
+    $ordering = Ordering::select("id","product_id",'supplier_id',"ordering_id",'amount','created_at','status')->get();
+      $suppliers = [];
+      $products = [];
+      $count = 0;
+      foreach ($ordering as $order) {
+          $suppliers[] = Supplier::select('name')->where('id', $order->supplier_id)->first();
+      }
+      foreach ($ordering as $order)
+      {
+          $products[] = Product::select('name')->where('id', $order->product_id)->first();
+      }
+    return view('admin.ordering.index', [
+      'ordering' => $ordering,
+        'products' => $products,
+        'suppliers' => $suppliers,
+        'count' => $count
     ]);
   }
 }
