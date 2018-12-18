6d127c8bd3ef8d1af925667aad29434674dc6fff
takashi <takashioicsn@gmail.com>
2018-12-18 20:35:23 +0900

    出庫一覧 作成

--- app/Http/Controllers/Admin/ShipmentController.php
+++ app/Http/Controllers/Admin/ShipmentController.php
@@ -5,15 +5,48 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Shipment;
+use App\Models\Product;
+use App\Models\User;
 
 class ShipmentController extends Controller
 {
   public function index()
   {
-    $shipments = Shipment::select('id','shipment_id','product_id','user_id','amount','status','created_at')->get();
+    $data = $product_id = $product_name = $product = $status = [];
+    $shipments = Shipment::select('id', 'shipment_id', 'user_id', 'product_id', 'amount', 'status', 'created_at')->get();
+    $shipments_id = Shipment::distinct()->select('shipment_id', 'user_id', 'created_at', 'status')->get();
 
+    foreach ($shipments as $shipment) {
+      $product_id[] = [
+        'shipment_id' => $shipment->shipment_id,
+        'product_name' => Product::where('id', $shipment->product_id)->first()->name,
+        'amount' => $shipment->amount,
+        'sales' => Product::where('id', $shipment->product_id)->first()->sales_price * $shipment->amount,
+      ];
+    }
+
+    foreach($product_id as $p) {
+      $product[] = $product_name[$p['shipment_id'] - 1] = ['name' => [], 'amount' => [], 'sales' => []];
+      array_push($product[$p['shipment_id'] - 1]['name'], $p['product_name']);
+      array_push($product[$p['shipment_id'] - 1]['amount'], $p['amount']);
+      array_push($product[$p['shipment_id'] - 1]['sales'], $p['sales']);
+    }
+
+    foreach ($shipments_id as $shipment_id) {
+      $data[] = [
+        'id' => $shipment_id->shipment_id,
+        'user_name' => User::where('id', $shipment_id->user_id)->first()->name,
+        'postal' => User::where('id', $shipment_id->user_id)->first()->postal,
+        'address' => User::where('id', $shipment_id->user_id)->first()->address,
+        'product_name' => $product[$shipment_id->shipment_id - 1]['name'],
+        'amount' => $product[$shipment_id->shipment_id - 1]['amount'],
+        'sales' => $product[$shipment_id->shipment_id - 1]['sales'],
+        'created_at' => $shipment_id->created_at->format('Y/m/d'),
+        'status' => $shipment_id->status
+      ];
+    }
     return view('admin.shipment.index', [
-      'shipments' => $shipments
+      'data' => $data
     ]);
   }
 }
--- resources/views/admin/shipment/index.blade.php
+++ resources/views/admin/shipment/index.blade.php
@@ -4,42 +4,76 @@
 <h1>出庫一覧</h1>
 
 <table class="table">
-
     <thead>
     <tr>
         <th scope="col">NO</th>
+        <th scope="col">会員名</th>
         <th scope="col">出庫日</th>
-        <th scope="col"> </th>
-
+        <th scope="col"></th>
     </tr>
     </thead>
     <tbody>
-    @foreach($shipments as $shipment)
-    <tr>
-        <th scope="row">{{$shipment->id}}</th>
-        <td>{{$shipment->created_at}}</td>
-        <td><a href data-toggle="modal" data-target="#OrderModal{{ $shipment->id }}">詳細</a></td>
-        <div class="modal fade bd-example-modal-lg" id="OrderModal{{ $shipment->id }}" tabindex="-1" role="dialog" aria-labelledby="OrderModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="OrderModalCenterTitle">出庫詳細</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div></div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+        @foreach($data as $shipment)
+            <tr>
+                <td>{{ $shipment['id'] }}</td>
+                <td>{{ $shipment['user_name'] }}</td>
+                <td>{{ $shipment['created_at'] }}</td>
+                <td><a href data-toggle="modal" data-target="#OrderModal{{ $shipment['id'] }}">詳細</a></td>
+                <div class="modal fade bd-example-modal-lg" id="OrderModal{{ $shipment['id'] }}" tabindex="-1" role="dialog" aria-labelledby="OrderModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="OrderModalCenterTitle">出庫詳細</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="row mb-3">
+                                    <div class="col-auto">注文番号</div>
+                                    <div class="col-auto">{{ $shipment['id'] }}</div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-auto">注文日</div>
+                                    <div class="col-auto">{{ $shipment['created_at'] }}</div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-auto">会員名</div>
+                                    <div class="col-auto">{{ $shipment['user_name'] }}</div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-auto">郵便番号</div>
+                                    <div class="col-auto">{{ $shipment['postal'] }}</div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-auto">住所</div>
+                                    <div class="col-auto">{{ $shipment['address'] }}</div>
+                                </div>
+                                <div class="mb-3">
+                                    <div class="row mb-1">
+                                        <div class="col">商品名</div>
+                                        <div class="col">注文数</div>
+                                    </div>
+                                    @for($i = 0; $i < count($shipment['product_name']); $i++)
+                                        <div class="row mb-1">
+                                            <div class="col">{{ $shipment['product_name'][$i] }}</div>
+                                            <div class="col">{{ $shipment['amount'][$i] }}冊</div>
+                                        </div>
+                                    @endfor
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-auto">売上</div>
+                                    <div class="col-auto">{{ array_sum($shipment['sales']) }}円</div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </tr>
+            </tr>
+        @endforeach
     </tbody>
-    @endforeach
 </table>
-
 @endsection
