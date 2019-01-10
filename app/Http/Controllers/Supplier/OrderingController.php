0677d1cdc9ba9ba15e667e146272adc7d0ba6fa5
takashi <takashioicsn@gmail.com>
2019-01-11 01:25:36 +0900

    入庫先 発注一覧

--- app/Http/Controllers/Supplier/OrderingController.php
+++ app/Http/Controllers/Supplier/OrderingController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Supplier;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Supplier;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Ordering;
+use App\Models\Product;
 
 class OrderingController extends Controller
 {
@@ -14,6 +18,26 @@ class OrderingController extends Controller
 
     public function index()
     {
-      return view('supplier.ordering.index');
+      $count = 1;
+      $data = [];
+      $orderings = Ordering::where('supplier_id', Auth::guard('supplier')->user()->id)->get();
+      foreach ($orderings as $ordering) {
+        $data[] = [
+          'id' => $count++,
+          'ordering_id' => $ordering->id,
+          'product_id' => $ordering->product_id,
+          'product_name' => Product::where('id', $ordering->product_id)->first()->name,
+          'amount' => $ordering->amount,
+          'admin_company_name' => env('USER_COMPANY_NAME'),
+          'admin_email' => env('USER_EMAIL'),
+          'admin_postal' => env('USER_POSTAL'),
+          'admin_address' => env('USER_ADDRESS'),
+          'status' => $ordering->status,
+          'created_at' => $ordering->created_at,
+        ];
+      }
+      return view('supplier.ordering.index', [
+        'data' => $data
+      ]);
     }
 }
--- resources/views/supplier/ordering/index.blade.php
+++ resources/views/supplier/ordering/index.blade.php
@@ -2,76 +2,84 @@
 
 @section ('content')
 <h1>発注一覧</h1>
-
-<!-- Modal -->
-<div class="modal fade bd-example-modal-lg" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">発注詳細</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                発注日 １２月１日<br>
-                発注コード:<br>
-                商品コード: 203<br>
-                商品名: 羅生門<br>
-                個数:　１<br>
-                発注先名: 上本町屋<br>
-                発注先住所: 〒０００-００００　大阪府～<br>
-                合計金額: １０００円<br>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
-                <button type="button" class="btn btn-primary">出庫する</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-
 <table class="table">
     <thead>
     <tr>
-        <th scope="col">日付</th>
-        <th scope="col">発注コード</th>
-        <th scope="col">発注先</th>
+        <th scope="col">発注番号</th>
+        <th scope="col">商品名</th>
+        <th scope="col">ISBN</th>
+        <th scope="col">数量</th>
+        <th scope="col">発注書店</th>
+        <th scope="col">発注日</th>
         <th scope="col"></th>
     </tr>
     </thead>
     <tbody>
-    <tr>
-        <th scope="row"></th>
-        <td></td>
-        <td></td>
-        <td><a href data-toggle="modal" data-target="#exampleModalCenter">
-                <ins>詳細</ins>
-            </a></td>
-    </tr>
-    <tr>
-        <th scope="row"></th>
-        <td></td>
-        <td></td>
-        <td><a href data-toggle="modal" data-target="#exampleModalCenter">
-                <ins>詳細</ins>
-            </a></td>
-    </tr>
-    <tr>
-        <th scope="row"></th>
-        <td></td>
-        <td></td>
-        <td><a href data-toggle="modal" data-target="#exampleModalCenter">
-                <ins>詳細</ins>
-            </a></td>
-    </tr>
+    @foreach($data as $ordering)
+        <tr>
+            <td>{{ $ordering['id'] }}</td>
+            <td>{{ $ordering['product_name'] }}</td>
+            <td>{{ $ordering['product_id'] }}</td> {{-- TODO:ISBNに --}}
+            <td>{{ $ordering['amount'] }}</td>
+            <td>{{ $ordering['admin_company_name'] }}</td>
+            <td>{{ $ordering['created_at']->format('Y/m/d') }}</td>
+            <td>
+                <a href data-toggle="modal" data-target="#OrderingModal{{ $ordering['id'] }}">詳細</a>
+            </td>
+        </tr>
+        <!-- Modal -->
+        <div class="modal fade bd-example-modal-lg" id="OrderingModal{{ $ordering['id'] }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">発注詳細</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-3">発注番号</div>
+                            <div class="col-9">{{ $ordering['id'] }}</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-3">発注日</div>
+                            <div class="col-9">{{ $ordering['created_at']->format('Y/m/d') }}</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-3">商品名</div>
+                            <div class="col-9">{{ $ordering['product_name'] }}</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-3">ISBN</div>{{-- TODO:ISBNに --}}
+                            <div class="col-9">{{ $ordering['product_id'] }}</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-3">数量</div>
+                            <div class="col-9">{{ $ordering['amount'] }}</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-3">発注書店</div>
+                            <div class="col-9">{{ $ordering['admin_company_name'] }}</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-3">郵便番号</div>
+                            <div class="col-9">{{ $ordering['admin_postal'] }}</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-3">住所</div>
+                            <div class="col-9">{{ $ordering['admin_address'] }}</div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                        <button type="button" class="btn btn-primary">出庫する</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
     </tbody>
 </table>
-<tr>
-
-    <a href="">出庫済み一覧</a>
-
-</tr>
 @endsection
 
