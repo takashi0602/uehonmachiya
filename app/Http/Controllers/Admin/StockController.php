06e37d3c578b4331049e40037b505511a2fdbcb6
takashi <takashioicsn@gmail.com>
2019-01-17 17:17:17 +0900

    在庫一覧 検索機能

--- app/Http/Controllers/Admin/StockController.php
+++ app/Http/Controllers/Admin/StockController.php
@@ -12,57 +12,87 @@ class StockController extends Controller
 {
     public function index(Request $request)
     {
-        $data = [];
-        // TODO: switch文から
-        /*if($request->status) {
-          $stocks = Stock::all();
-          switch ($request->status) {
-            case 1:
-              foreach ($stocks as $stock) {
-                if($stock->amount >= $stock->safety) {
-
-                }
-              }
-              break;
-            case 2:
-              $stocks = Stock::where()->get();
-              break;
-            case 3:
-              $stocks = Stock::where()->get();
-              break;
-            case 4:
-              $stocks = Stock::where()->get();
-              break;
-            case 5:
-              $stocks = Stock::where()->get();
-              break;
-            case 6:
-              $stocks = Stock::where()->get();
-              break;
-            case 7:
-              $stocks = Stock::all();
-              break;
-          }
+        $data = $box = [];
+        if($request->status) {
+            $stocks = Stock::all();
+            switch ($request->status) {
+                case 1:
+                    foreach ($stocks as $stock) {
+                        if($stock->amount >= $stock->safety) {
+                            $box[] = $stock;
+                        }
+                    }
+                    break;
+                case 2:
+                    foreach ($stocks as $stock) {
+                        if($stock->safety - $stock->amount > 0 && $stock->safety - $stock->amount < $stock->safety) {
+                            $box[] = $stock;
+                        }
+                    }
+                    break;
+                case 3:
+                    foreach ($stocks as $stock) {
+                        if($stock->amount ==  0) {
+                            $box[] = $stock;
+                        }
+                    }
+                    break;
+                case 4:
+                    foreach ($stocks as $stock) {
+                        if($stock->amount > 0) {
+                            $box[] = $stock;
+                        }
+                    }
+                    break;
+                case 5:
+                    foreach ($stocks as $stock) {
+                        if($stock->amount < $stock->safety) {
+                            $box[] = $stock;
+                        }
+                    }
+                    break;
+                case 6:
+                    foreach ($stocks as $stock) {
+                        if($stock->amount >= $stock->safety || $stock->amount == 0) {
+                            $box[] = $stock;
+                        }
+                    }
+                    break;
+                case 7:
+                    $box = Stock::all();
+                    break;
+            }
+            foreach ($box as $b) {
+                $product = Product::where('id', $b->product_id)->first();
+                $data[] = [
+                    'id' => $b->id,
+                    'product_id' => $b->product_id,
+                    'name' => $product->name,
+                    'price' => $product->price,
+                    'supplier' => Supplier::where('id', $product->supplier_id)->first()->name,
+                    'amount' => $b->amount,
+                    'safety' => $b->safety
+                ];
+            }
         } else {
-          $stocks = Stock::all();
-        }*/
-        $stocks = Stock::all();
-        foreach ($stocks as $stock) {
-          $product = Product::where('id', $stock->product_id)->first();
-          $data[] = [
-            'id' => $stock->id,
-            'product_id' => $stock->product_id,
-            'name' => $product->name,
-            'price' => $product->price,
-            'supplier' => Supplier::where('id', $product->supplier_id)->first()->name,
-            'amount' => $stock->amount,
-            'safety' => $stock->safety
-          ];
+            $stocks = Stock::all();
+            foreach ($stocks as $stock) {
+                $product = Product::where('id', $stock->product_id)->first();
+                $data[] = [
+                    'id' => $stock->id,
+                    'product_id' => $stock->product_id,
+                    'name' => $product->name,
+                    'price' => $product->price,
+                    'supplier' => Supplier::where('id', $product->supplier_id)->first()->name,
+                    'amount' => $stock->amount,
+                    'safety' => $stock->safety
+                ];
+            }
+        }
+        return view('admin.stock', [
+            'data' => $data
+        ]);
     }
-    return view('admin.stock', [
-        'data' => $data
-    ]);
-}
 }
 
 
